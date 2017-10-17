fa7b7a1521490200eb1752f72269744570b7555d
Russell Wolf <russhwolf@users.noreply.github.com>
2017-10-17 15:48:36 -0400

    Refactor TimberConfig to be more idiomatic (#11)

--- app/src/main/kotlin/io/intrepid/skotlinton/logging/TimberConfig.kt
+++ app/src/main/kotlin/io/intrepid/skotlinton/logging/TimberConfig.kt
@@ -6,29 +6,25 @@ import timber.log.Timber
 
 object TimberConfig {
     fun init(crashReporter: CrashReporter) {
-        var tree: Timber.Tree? = null
-        if (BuildConfig.LOG_CONSOLE) {
-            if (BuildConfig.REPORT_CRASH) {
-                tree = object : Timber.DebugTree() {
-                    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                        if (priority >= Log.INFO) {
-                            crashReporter.log(priority, tag, message)
-                        } else {
-                            super.log(priority, tag, message, t)
-                        }
+        val tree: Timber.Tree? = when {
+            BuildConfig.LOG_CONSOLE && BuildConfig.REPORT_CRASH -> object : Timber.DebugTree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    if (priority >= Log.INFO) {
+                        crashReporter.log(priority, tag, message)
+                    } else {
+                        super.log(priority, tag, message, t)
                     }
                 }
-            } else {
-                tree = Timber.DebugTree()
             }
-        } else if (BuildConfig.REPORT_CRASH) {
-            tree = object : Timber.Tree() {
+            BuildConfig.LOG_CONSOLE -> Timber.DebugTree()
+            BuildConfig.REPORT_CRASH -> object : Timber.Tree() {
                 override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
                     if (priority >= Log.INFO) {
                         crashReporter.log(priority, tag, message)
                     }
                 }
             }
+            else -> null
         }
         if (tree != null) {
             Timber.plant(tree)
