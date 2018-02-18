ca45c35a56182edc32acc9408e9a8523e30524ae
Russell <russhwolf@gmail.com>
2018-02-18 12:57:46 -0500

    Move Example1Presenter to common

--- common/src/main/kotlin/io/intrepid/multiskeleton/screens/example1/Example1Presenter.kt
+++ common/src/main/kotlin/io/intrepid/multiskeleton/screens/example1/Example1Presenter.kt
@@ -3,7 +3,7 @@ package io.intrepid.multiskeleton.screens.example1
 import io.intrepid.multiskeleton.base.BasePresenter
 import io.intrepid.multiskeleton.base.PresenterConfiguration
 
-internal class Example1Presenter(view: Example1Contract.View, configuration: PresenterConfiguration)
+class Example1Presenter(view: Example1Contract.View, configuration: PresenterConfiguration)
     : BasePresenter<Example1Contract.View>(view, configuration), Example1Contract.Presenter {
 
     override fun onButtonClick() {
